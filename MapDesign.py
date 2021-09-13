024b3d73f717ec8810b14e36245689c5026b2b2c
Txndra <seandougan04@gmail.com>
2021-09-13 19:46:35 +0100

    e

--- MapDesign.py
+++ MapDesign.py
@@ -6,21 +6,10 @@ import pickle
 
 
 
-class Map:
-    def __init__(self):
-        self.mapSize = []
-        
-    
 
-    def getSize(self):
-        messageBox = tk.Tk()
-        sizeMessage = tk.Label(
-        text = "ENTER MAP SIZE",
-        )
-        getWidth = tk.Entry(messageBox)
-        sizeMessage.pack()
-        getWidth.pack()
-        messageBox.mainloop()
+
+
+
 
 
 
