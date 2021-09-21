1b9700412fc8a3db15aeda9beeb3f149fea537c3
Txndra <seandougan04@gmail.com>
2021-09-21 12:36:22 +0100

    e

--- MapDesign.py
+++ MapDesign.py
@@ -6,6 +6,7 @@ import pickle
 import os
 from typing import Type
 from Stack import Stack 
+import keyboard
 
 
 
@@ -80,8 +81,17 @@ class MapCreation(tk.Frame):
         clearButton = tk.Button(optionsFrame, text = 'CLEAR ALL', command = self.clearMap).grid(row = 1 , column = 0)
         saveButton = tk.Button(optionsFrame, text = 'SAVE', command = self.saveMap).grid(row = 1 , column = 1)
 
+        messagebox.showinfo("Saving Map","If the map size is too big and you cannot see the save button, press 's' to save.")
+
         #Some options for creating the map
         
+
+    def checkKeyboardInput(self):
+        if keyboard.read_key() == "s":
+            self.saveMap
+
+
+
     def startButton(self):
         buttonDeya = False
         for i in self.buttons:
