fea4dcd8342cd761b18e9a9be2d5a235934be61f
Sk <86353559+Txndra@users.noreply.github.com>
2021-09-28 13:25:22 +0100

    Update neural.py

--- neural.py
+++ neural.py
@@ -2,10 +2,16 @@ import sys
 import numpy as np
 import matplotlib
 
-inputs = [1, 2, 3, 2.5]
+inputs = [[1, 2, 3, 2.5],
+          [2.0, 5.0, -1.0, 2.0 ],
+          [-1.5, 2.7, 3.3, -0.8]]
 
 
-weights = [[0.2, 0.8, -0.5, 1.0]
+weights = [[0.2, 0.8, -0.5, 1.0],
+           [0.5, -0.91, 0.26, -0.5],
+           [-0.26, -0.27, 0.17, 0.87]]
 
-bias = 2
-output = p.dot(weights, inputs) + bias
+biases = [2, 3, 0.5]
+
+output = np.dot(weights, inputs) + biases
+print(output)
