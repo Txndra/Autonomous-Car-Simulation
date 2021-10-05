cce7332cc87e898e66a911c82fa72b77d85463ec
Sk <86353559+Txndra@users.noreply.github.com>
2021-10-05 12:58:41 +0100

    Update neural.py

--- neural.py
+++ neural.py
@@ -9,9 +9,20 @@ inputs = [[1, 2, 3, 2.5],
 
 weights = [[0.2, 0.8, -0.5, 1.0],
            [0.5, -0.91, 0.26, -0.5],
-           [-0.26, -0.27, 0.17, 0.87]]
-
+           [-0.26, -0.27, 0.17, 0.87]]  
+           
+           
+#This would take the dot product of a (3,4) matrix by a (3,4) matrix, but this is not possible.
+#Instead we have to switch rows and columns of weights (use the TRANSPOSE) line 24
 biases = [2, 3, 0.5]
 
-output = np.dot(weights, inputs) + biases
-print(output)
+weights2 = [[0.1, -0.14, 0.5],
+           [-0.5, 0.12, -0.33],
+           [-0.44, 0.73, -0.13]]  
+
+biases2 = [-1, 2, -0.5]
+
+layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
+layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
+
+print(layer2_outputs)
