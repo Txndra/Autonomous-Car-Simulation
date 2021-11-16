92a56e6ce8249637e9fc0bf980e5200aaf75e8cb
Txndra <seandougan04@gmail.com>
2021-11-16 15:56:01 +0000

    e

--- neural.py
+++ neural.py
@@ -1,28 +1,34 @@
 import sys
 import numpy as np
-import matplotlib
+import matplotlib.pyplot as plt
 
-inputs = [[1, 2, 3, 2.5],
-          [2.0, 5.0, -1.0, 2.0 ],
-          [-1.5, 2.7, 3.3, -0.8]]
+class Neural:
+    def __init__(self):
+        self.inputLayerSize = 4
+        self.outputLayerSize = 2
+
+        self.weights1 = np.random.randn(self.inputLayerSize, self.outputLayerSize) #randomises weights
+
+    def calculateOutput(self, inputs):
+        e = np.array([inputs])
+        output = self.forward(e)[0] #determines outputs
+        angle = output[0]
+        if angle > 0.6:
+            angleChange = -2
+        elif angle < 0.4:
+            angleChange = 2
+        else:
+            angleChange = 0
+
+        acceleration = output[1]
+
+        return(angleChange, acceleration)
 
 
-weights = [[0.2, 0.8, -0.5, 1.0],
-           [0.5, -0.91, 0.26, -0.5],
-           [-0.26, -0.27, 0.17, 0.87]]  
-           
-           
-#This would take the dot product of a (3,4) matrix by a (3,4) matrix, but this is not possible.
-#Instead we have to switch rows and columns of weights (use the TRANSPOSE) line 24
-biases = [2, 3, 0.5]
+    def forward(self, X): #Traverses through neyral network using matrix multiplication to find an output
+        pass
 
-weights2 = [[0.1, -0.14, 0.5],
-           [-0.5, 0.12, -0.33],
-           [-0.44, 0.73, -0.13]]  
+    def sigmoid(self, x):
+        return 1/(1 + np.exp(-x))
 
-biases2 = [-1, 2, -0.5]
 
-layer1_outputs = np.dot(inputs, np.array(weights).T) + biases
-layer2_outputs = np.dot(layer1_outputs, np.array(weights2).T) + biases2
-
-print(layer2_outputs)
