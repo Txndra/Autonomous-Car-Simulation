a4589fd9d9f018c77c67c0771228dc5d539ed8e0
Txndra <seandougan04@gmail.com>
2021-12-07 13:05:46 +0000

    e

--- car.py
+++ car.py
@@ -1,8 +1,42 @@
 from neural import Neural
 import pygame
 from pygame.math import Vector2
-from radar import radars
+from prototype import CAR_SIZE_X, CAR_SIZE_Y
 
 class Car:
     beamAngles = [-45, 0, 45] #angles for the car beams
-    
+    carSize = None
+    beamCarOffset = 10
+    idCounter = 0
+    CAR_SIZE_X = 20
+    CAR_SIZE_Y = 20
+    def __init__(self):
+        self.id = Car.idCounter
+        Car.idCounter += 1
+
+        self.brain = Neural() # Assigns neural network to car
+        self.framesAlive = 0
+        self.fitness = 0
+        self.dead = False
+
+        self.bestCar = False
+        self.collidedCheckPoints = []
+        self.sprite = pygame.image.load(r"car.png").convert()
+        self.sprite = pygame.transform.scale(self.sprite, (CAR_SIZE_X, CAR_SIZE_Y))
+        self.center = [self.position[0] + CAR_SIZE_X / 2, self.position[1] + CAR_SIZE_Y / 2]
+        self.rotated_sprite = self.sprite
+
+        self.angle = 0
+        self.speed = 0
+        self.radars = []
+        self.drawing_radars = []
+        
+        def draw_radar(self, screen):
+        # Optionally Draw All Sensors / Radars
+            for radar in self.radars:
+                position = radar[0]
+                pygame.draw.line(screen, (0, 255, 0), self.center, position, 1)
+                pygame.draw.circle(screen, (0, 255, 0), position, 5)
+
+
+
--- simulation.py
+++ simulation.py
@@ -5,13 +5,131 @@ import tkinter
 import pickle
 import os
 from tkinter.filedialog import asksaveasfile
+import re #regular expression
+
+class Tile:
+    __size = 10
+    tileID = 0
+
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.tileID = Tile.tileID
+        Tile.tileID += 1
+
+    def getSize():
+        return Tile.__size
+    
+    def setSize(size):
+        if Tile.__size is not None:
+            Tile.__size = size
+
+class Wall(Tile):
+    colour = (255,0,0)
+    def show(self, SCR):
+        pygame.draw.rect(SCR, Wall.colour, (self.x, self.y, Tile.getSize(), Tile.getSize()))
+        pygame.draw.rect(SCR, (255, 255, 255), (self.x, self.y, Tile.getSize(), Tile.getSize()),1)
 
 
-class Simulation():
-    pass
+class Track(Tile):
+    colour = (0,255,255)
+    def show(self, SCR):
+        pygame.draw.rect(SCR, Wall.colour, (self.x, self.y, Tile.getSize(), Tile.getSize()))
+        pygame.draw.rect(SCR, (255, 255, 255), (self.x, self.y, Tile.getSize(), Tile.getSize()),1)
+
+
+class statsBox:
+    def __init__(self, x, y, w, h):
+        self.x = x
+        self.y = y
+        self.w = w #width
+        self.h = h #height
+
+        pygame.font.init()
+        self.font = pygame.font.SysFont('Comic Sans MS', 10)
+
+    def show(self, SCR, bestFitness, generationNum):
+        bestFitnessText = self.font.render("Best fitness: ", + str(bestFitness), False, (0,0,0))
+        generationNumtext = self.font.render("Current gen: " + str(generationNum), False, (0,0,0))
+
+        pygame.draw.rect(SCR, (220,220,220), (self.x, self.y, self.w, self.h))
+
+        SCR.blit(bestFitnessText, (self.x + 10, self.y + 10))
+        SCR.blit(generationNumtext, (self.x + 10, self.y + 50))
+
+class Simulation:
+    def __init__(self, MapDict, mutation, loadedWeights):
+        pygame.init()
+
+        SCREEN_W = pygame.display.Info().current_w
+        SCREEN_H = pygame.display.Info().current_h
+
+
+        (self.W, self.H) = self.setWindowSize(MapDict, SCREEN_W-100, SCREEN_H-100)
+
+        self.SCR = pygame.display.set_mode((self.W, self.H))
+        self.SCR.set_alpha(0)
+
+        pygame.display.set_caption('grid') #caption for window
+        self.fpsClock = pygame.time.Clock()
+        self.SCR.fill((0,255,0), rect = None) #screen col
+
+        #info section
+        pygame.draw.rect(self.SCR, (220,220,220), (0, (self.H - Tile.getSize()), self.W - Tile.getSize()))
+        self.statsPane = statsBox(0, (self.H - Tile.getSize()), self.W - Tile.getSize())
+
+
+        (self.walls, self.tracks) = Simulation.generateMap(MapDict)
+
+        for w in self.walls:
+            w.show(self.SCR)
+        for t in self.tracks:
+            t.show(self.SCR)
+
+
+        startTile = self.tracks[MapDict["startID"]]
+
+    def generateMap(MapDict):
+        rows = MapDict["rows"]
+        columns = MapDict["columns"]
+        mapRLE = MapDict["data"]
+
+        mapRLEarray = re.split(r'(\d+)', mapRLE)[1:]
+        mapRLE2Darray = []
+        for i in range(0, len(mapRLEarray), 2):
+            mapRLE2Darray.append([mapRLEarray[i], mapRLEarray[i+1]])
+
+
+        #create tiles
+        walls = []
+        tracks = []
+        x = 0
+        y = 0
+        for group in mapRLE2Darray: 
+            for e in range(int(group[0])):
+                if x > Tile.getsize()*(columns - 1):
+                    x = 0
+                    y += Tile.getsize()
+
+                if group[1] == 'W':
+                    walls.append(Wall(x,y))
+                elif group[1] == 'T':
+                    tracks.append(Track(x,y))
+
+                x += Tile.getsize()
+        return (walls, tracks)
+
+    def setWindowSize(self, MapDict, devW, devH):
+        tileSize = 0
+        while tileSize*((MapDict["rows"] + 1)) < devH and tileSize*(MapDict["columns"]) < devW:
+            tileSize += 1
+        tileSize -= 1
+        Tile.setSize(tileSize)
+        return (tileSize * MapDict["columns"], tileSize * (MapDict["rows"] + 1))
 
 
 
+yo = Simulation(r'C:\Users\sdgam\OneDrive\Documents\AUTONOMOUS CARSIM\A-Level-NEA---Autonomous-Car\maps\map1.pkl', 20, None)
 
     
 
